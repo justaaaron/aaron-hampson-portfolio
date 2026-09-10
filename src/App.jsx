c01d4d359069aac50a1bd04fd8b329646af9097d
justaaaron <aaronhampson24@gmail.com>
2026-09-10 19:49:34 +0100

    Skeleton-ing general sections of portfolio.

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,30 @@ function Hero() {
   )
 }
 
+function AboutMe() {
+  return (
+    <div></div>
+  )
+}
+
+function Experience() {
+  return (
+    <div></div>
+  )
+}
+
+function Projects() {
+  return (
+    <div></div>
+  )
+}
+
+function Contact() {
+  return (
+    <div></div>
+  )
+}
+
 function App() {
   return (
     <Hero />
